550246961b4bfb90f252eb10b8d55454000da07b
Aleksandr.KHolin <Aleksandr.KHolin@norbit.ru>
2026-02-18 11:28:37 +0300

    feat(lecture): implement 18 02 2026

---
+++ local/js/aholincrmcustomtab/tools/popup/config.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+    die();
+
+return [
+    'js' => [
+        'src/js/main.js'
+    ],
+    'css' => [
+        'src/css/main.css'
+    ],
+    'rel' => [
+        'popup',
+        // 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.10.0/js/bootstrap-datepicker.min.js' - пример подключения сторонней библиотеки
+    ],
+];
